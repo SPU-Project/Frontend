39f7fad36e7a835fa59d55f50bb47cddd87bc3fc
zakyahmadfaisal <zakyahmad@apps.ipb.ac.id>
2024-10-24 16:09:54 +0700

    new header status

--- src/components/FormStatusProduct.jsx
+++ src/components/FormStatusProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Select from "react-select";
-import AdminHeader from "./AdminHeader"; // Import AdminHeader
+import AdminHeader from "./AdminHeader(Status)"; // Import AdminHeader
 import "../styles/FormStatusProduct.css"; // Import CSS file
 
 function FormStatusProduct() {
